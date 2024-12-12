1b6de60268199ed7a4b077afeb68680825e046c5
noelnp <noel.peltomaki@gmail.com>
2024-12-12 15:09:24 +0100

    Lägger till metoder och konstruktor för CasinoApp

--- src/CasinoApp.java
+++ src/CasinoApp.java
@@ -1,2 +1,50 @@
 public class CasinoApp {
+
+    UserManager userManager;
+
+    public CasinoApp () {
+        userManager = new UserManager();
+    }
+
+    // Logga in
+    public void logInUser(String userName, String password) {
+
+    }
+
+    // Registrera konto
+    public void registerUser(String userName, String password) {
+
+    }
+
+    // Logga ut
+    public void logOutUser() {
+
+    }
+
+    // Uttag
+    public void withdraw(double amount) {
+
+    }
+
+    // Insättning
+    public void deposit(double amount) {
+
+    }
+
+    // Sätt insättningsgräns
+    public void setDepositLimit() {
+
+    }
+
+    // Gå in i spel
+    public void enterGame() {
+
+    }
+
+    // Gå ut ur spel
+    public void quitGame() {
+
+    }
+
+
 }
