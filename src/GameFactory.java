ba58e651e9fee528ea6bcd8c552fd2ffc9eaa216
noelnp <noel.peltomaki@gmail.com>
2024-12-16 17:33:31 +0100

    Remove SlotsGame class

--- src/GameFactory.java
+++ src/GameFactory.java
@@ -2,7 +2,6 @@ public class GameFactory {
 
     public Game getGame(GameType game) {
         return switch (game) {
-            case SLOTS -> new SlotsGame();
             case ROULETTE -> new RouletteGame();
             case BLACKJACK -> new BlackJackGame();
         };
